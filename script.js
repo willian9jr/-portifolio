7f17d6f1d8901eac33fd2ddbad460db60700df8c
Willian Jr <willian9jr@hotmail.com>
2022-05-16 19:05:43 -0300

    atualizando versao desktop

--- script.js
+++ script.js
@@ -1,65 +1,89 @@
-const navMenu = document.querySelector('.nav-menu');
-const menuMobile = document.querySelector('.menu-mobile')
-const menuItem = document.querySelectorAll('.menu-item');
+const navMenu = document.querySelector(".nav-menu");
+const menuMobile = document.querySelector(".menu-mobile");
+const menuItem = document.querySelectorAll(".menu-item");
 const contact = document.querySelector("#contact");
 const about = document.querySelector("#about");
 const projects = document.querySelector("#projects");
+const footer = document.querySelector(".footer");
+const vitrine = document.querySelector(".grid-vitrine");
+const menuX = document.querySelector("#menu-x");
 
-
-navMenu.addEventListener("click", () =>{
+navMenu.addEventListener("click", () => {
     navMenu.classList.toggle("active");
-    if(menuMobile.style.width == "230px"){
-        menuMobile.style.width = "0"
-    }else{
-        menuMobile.style.width = "230px"
+    if (menuMobile.style.width == "150px") {
+        menuMobile.style.width = "0";
+        vitrine.style.opacity= "1";
+    } else {
+        menuMobile.style.width = "150px";
+        vitrine.style.opacity = "0.1";
     }
-})
+});
 
-function menuActive(n){
-
-    if(menuItem[n] == menuItem[1]){
-        contact.style.display = "none";
-        projects.style.display = "none";
-        if(about.style.display == "block"){
-            about.style.display = "none";
-        }else{
-            about.style.display = "block";
-        }  
-    }else if(menuItem[n] == menuItem[2]){ 
-        contact.style.display = "none";
-        about.style.display = "none";
-        if(projects.style.display == "block"){
-            projects.style.display = "none";
-        }else{
-            projects.style.display = "block";
-        }
-    }else if(menuItem[n] == menuItem[3]){
-        about.style.display = "none";
-        projects.style.display = "none";
-        if(contact.style.display == "block"){
+function menuActive(n) {
+    switch (menuItem[n]) {
+        case menuItem[1]:
             contact.style.display = "none";
-        }else{
-            contact.style.display = "block";
-        }
-    }else{
-        contact.style.display = "none";
-        about.style.display = "none";
-        projects.style.display = "none";
+            projects.style.display = "none";
+            footer.style.display = 'none';
+            if (about.style.display == "block") {
+                about.style.display = "none";
+            } else {
+                about.style.display = "block";
+            }
+            break;
+        case menuItem[2]:
+            contact.style.display = "none";
+            about.style.display = "none";
+            footer.style.display = 'none';
+            if (projects.style.display == "block") {
+                projects.style.display = "none";
+            } else {
+                projects.style.display = "block";
+            }
+            break;
+        case menuItem[3]:
+            about.style.display = "none";
+            projects.style.display = "none";
+            footer.style.display = 'none';
+            if (contact.style.display == "block") {
+                contact.style.display = "none";
+            } else {
+                contact.style.display = "block";
+            }
+            break;
+        default:
+            contact.style.display = "none";
+            about.style.display = "none";
+            projects.style.display = "none";
+            break;
     }
 
-
-    for(let i in menuItem){
-        if(menuItem[i] !== menuItem[n]){
-            menuItem[i].classList.remove("menu-active");
-        }else{
-            menuItem[n].classList.add("menu-active");
-        }
-    }
+    // for (let i in menuItem) {
+    //     if (menuItem[i] !== menuItem[n]) {
+    //         menuItem[i].classList.remove("menu-active");
+    //     } else {
+    //         menuItem[n].classList.add("menu-active");
+    //     }
+    // }
 }
 
-function close(){
+function closeModal() {
+    // if(menuX.style.display == "none"){
+    //     footer.style.display = 'flex';
+    // }
+    
     contact.style.display = "none";
-    about.style.display = "none";
-    projects.style.display = "none";
-}
+    if (contact.classList == "menu-active") {
+        contact.classList.remove("menu-active");
+    }
 
+    about.style.display = "none";
+    if (about.classList == "menu-active") {
+        about.classList.remove("menu-active");
+    }
+
+    projects.style.display = "none";
+    if (projects.classList == "menu-active") {
+        projects.classList.remove("menu-active");
+    }
+}
